82ce71bc38da55271019252251bdb1009c120242
Nathan Hai Nguyen <nathahn9@uci.edu>
2025-05-30 06:01:13 +0000

    train()

--- DQN.py
+++ DQN.py
@@ -92,8 +92,9 @@ class ReplayMemory:
         return len(self.memory)
     
 class Agent:
-    def __init__(self, env: DQNEnv, learning_rate: float, discount_factor: float, epsilon: float, 
-                in_states: int, h1_nodes: int, h2_nodes: int, maxlen: int):
+    def __init__(self, env: DQNEnv, learning_rate: float, discount_factor: float, epsilon: float,
+                batch_size: int, sync_rate: int, in_states: int, h1_nodes: int, h2_nodes: int, 
+                maxlen: int):
         """
         Initializes the AI Agent with provided hyper parameters and DQN info
 
@@ -102,6 +103,8 @@ class Agent:
             learning_rate (float): Alpha learning rate for optimizer
             discount_factor (float): Gamma discount factor for Q calculation
             epsilon (float): Epsilon-greedy action selection
+            batch_size (int): Size of sample batch from ReplayMemory
+            sync_rate (int): When to sync policy_dqn and target_dqn
             in_states (int): Size of input vector
             h1_nodes (int): Number of nodes in first hidden layer
             h2_nodes (int): Number of nodes in second hidden layer
@@ -114,10 +117,13 @@ class Agent:
         self.learning_rate = learning_rate # alpha
         self.discount_factor = discount_factor # gamma
         self.epsilon = epsilon # epsilon-greedy
+        self.batch_size = batch_size
+        self.sync_rate = sync_rate
 
         # Initialize DQN
         self.policy_dqn = DQN(in_states, h1_nodes, h2_nodes)
         self.target_dqn = DQN(in_states, h1_nodes, h2_nodes)
+        self.target_dqn.load_state_dict(self.policy_dqn.state_dict())
 
         # Initialize ReplayMemory
         self.memory = ReplayMemory(maxlen)
@@ -146,3 +152,80 @@ class Agent:
         else:
             q_values = self.policy_dqn(state.unsqueeze(0)) # Get q values from output layer
             return torch.argmax(q_values).item() # Return best move
+        
+    def train(self, episodes: int) -> None:
+        """
+        Trains the Agent on the level episodes amount of times, changing weights and optimizing as needed
+
+        Args:
+            episodes (int): Number of episodes to train on
+
+        Returns:
+            None
+        """
+        rewards_per_episode = np.zeros(episodes) # List to keep track of rewards collected per episode
+        epsilon_history = np.zeros(episodes) # List to keep track of epsilon decay
+
+        # Train for x episodes
+        for i in range(episodes):
+            state = self.env.reset()
+            done = False # True when puzzle is solved or exceeded step limit (in DNQEnv)
+            accumulated_reward = 0
+
+            # While puzzle hasnt been solved and havent exceeded step limit
+            while (not done):
+                # Select Action for current state
+                action = self.select_action(state)
+
+                # Make Action, get results
+                new_state, reward, done, info = self.env.step(action)
+                accumulated_reward += reward
+
+                # Append Transition to Memory
+                transition = Transition(state, action, reward, new_state, done)
+                self.memory.append(transition)
+
+                # Update State
+                state = new_state
+            
+            rewards_per_episode[i] = accumulated_reward
+
+            # Grab Sample Batch from Memory and Optimize
+            if len(self.memory) >= self.batch_size:
+                batch = self.memory.sample(self.batch_size)
+                self.optimize(batch)
+
+                # Sync policy_dqn and target_dqn
+                if self.step_counter >= self.sync_rate:
+                    self.step_counter = 0
+                    self.target_dqn.load_state_dict(self.policy_dqn.state_dict())
+            
+            # Decay Epsilon
+            self.epsilon = max(self.epsilon - (1/episodes), 0)
+            epsilon_history[i] = self.epsilon
+        
+        # Save Policy
+        torch.save(self.policy_dqn.state_dict(), 'dqn.pt')
+
+        # Save Results Figure
+        plt.figure(figsize=(12, 5))
+
+        # Track accumulater reward per episode
+        # Episode vs Accumulated Reward
+        plt.subplot(1, 2, 1)
+        plt.plot(rewards_per_episode)
+        plt.title("Accumulated Reward per Episode")
+        plt.xlabel("Episode")
+        plt.ylabel("Accumulated Reward")
+
+        # Track epsilon decay per episode
+        # Episode vs Epsilon
+        plt.subplot(1, 2, 2)
+        plt.plot(epsilon_history)
+        plt.title("Epsilon Decay per Episode")
+        plt.xlabel("Episode")
+        plt.ylabel("Epsilon")
+
+        plt.tight_layout()
+        plt.savefig("dqn_training_results.png")
+        plt.close()
